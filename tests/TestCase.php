0a8188afb8939c1ab9cf8666dad0e55a0ebbefe0
unknown <abdessamad11122@gmail.com>
2026-08-19 00:47:11 +0100

    test: isolate database and disable public registration

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,30 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use RuntimeException;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (! app()->environment('testing')) {
+            throw new RuntimeException(
+                'تم إيقاف الاختبارات: بيئة التطبيق ليست الاختبارات.'
+            );
+        }
+
+        if (config('database.default') !== 'sqlite') {
+            throw new RuntimeException(
+                'تم إيقاف الاختبارات: يجب استعمال سكل لايت وليس قاعدة التطوير.'
+            );
+        }
+
+        if (config('database.connections.sqlite.database') !== ':memory:') {
+            throw new RuntimeException(
+                'تم إيقاف الاختبارات: يجب أن تكون قاعدة  سكل لايت داخل الذاكرة.'
+            );
+        }
+    }
 }
